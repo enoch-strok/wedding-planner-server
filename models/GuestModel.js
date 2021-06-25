9952c58acf9686f73a52af6db244a28c02d13a39
Enoch <enochstrok@outlook.com>
2021-06-24 19:53:10 -0700

    updated fields in database

--- models/GuestModel.js
+++ models/GuestModel.js
@@ -5,6 +5,7 @@ const GuestSchema = new mongoose.Schema(
             guestName: { type: String },
             guestPhone: {type: Number},
             numberOfGuests: { type: Number},
+            status: { type: String },
             notes: { type: String},
         },
         {
